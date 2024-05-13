06dbb0bec347bde538d3c4151cf32e6a0d137a5b
Coultags <167041164+Coultags@users.noreply.github.com>
2024-05-13 16:52:37 -0600

    Add files via upload

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,9 @@
+var Sent1 =("This is the beginning of the string");
+var Sent2 =(" and this is the end of the string");
+
+window.alert("Hello World!"); //Window alert box
+document.write("Goodbye World!"); //Text on the webpage
+document.write("<br>");
+document.write(Sent1 + Sent2); //Calling both var
+document.write("<br>");
+document.write(56 * 90); //This is a expression
